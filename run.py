3720e7fc3ce5198ec3b5d9b584fca54acec67db9
informatykdmd <informatyk@dmdbudownictwo.pl>
2024-09-10 13:13:30 +0200

    Working now

--- run.py
+++ run.py
@@ -839,5 +839,5 @@ def subpage():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5080)
-    # app.run(debug=True, host='0.0.0.0', port=5080)
+    # app.run(debug=True, port=5080)
+    app.run(debug=True, host='0.0.0.0', port=5080)
